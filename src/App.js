8dca4e7be8f0e91f190bcec8b9be06aa20ee327a
CrackerJaps <seantokuzo@gmail.com>
2022-02-09 15:59:43 -0800

    Create start page elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
 function App() {
-  return (
-    <div>
+
+    const startPage = (
+    <div className="start-div">
+      <h1>Trivial Trivia</h1>
+      <h4>How much trivial knowledge do you possess</h4>
+      <button className="start-button">Start Game</button>
     </div>
+  )
+
+  return (
+    <main>
+      {startPage}
+    </main>
   );
 }
 
